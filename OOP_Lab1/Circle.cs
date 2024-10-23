9910be5d76c5ccf977c8b4f961807e7f0922fe42
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-10-23 10:28:39 +0200

    Add project files.

---
+++ OOP_Lab1/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab1
+{
+    internal class Circle
+    {
+        double Radius { get; set; }
+
+        public double GetArea()
+        {
+            double area = Radius * Radius * Math.PI;
+            Console.WriteLine($"The area of a circle with radius {Radius} cm is {area} cm²");
+            return area;
+        }
+    }
+
+}
+
