57d94dd92abfabfeea93ded1593ab94c1dbe8838
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-10-27 23:25:00 +0100

    Update Program.cs

--- OOP_Lab1/Program.cs
+++ OOP_Lab1/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+            var area1 = circle1.GetArea();
+            var area2 = circle2.GetArea();
         }
     }
 }
